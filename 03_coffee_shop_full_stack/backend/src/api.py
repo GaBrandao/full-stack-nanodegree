3f9768deb5a46dce28cdce3808438540ea14f223
Gabriel Brandão <gabriel.brandaoal@gmail.com>
2020-08-27 23:16:08 -0300

    Add endpoint handlers

--- 03_coffee_shop_full_stack/backend/src/api.py
+++ 03_coffee_shop_full_stack/backend/src/api.py
@@ -67,7 +67,7 @@ def get_drinks_detail(payload):
 
 
 '''
-@TODO implement endpoint
+@DONE implement endpoint
     POST /drinks
         it should create a new row in the drinks table
         it should require the 'post:drinks' permission
@@ -101,12 +101,12 @@ def post_drinks(payload):
 
     return jsonify({
         'success': True,
-        'drink': new_drink.long()
+        'drinks': [new_drink.long()]
     })
 
 
 '''
-@TODO implement endpoint
+@DONE implement endpoint
     PATCH /drinks/<id>
         where <id> is the existing model id
         it should respond with a 404 error if <id> is not found
@@ -118,8 +118,36 @@ def post_drinks(payload):
 '''
 
 
+@app.route('/drinks/<int:id>', methods=['PATCH'])
+@requires_auth('patch:drinks')
+def patch_drinks(payload, id):
+    drink = Drink.query.get_or_404(id)
+
+    data = request.get_json()
+
+    if not data:
+        abort(400)
+
+    title = data.get('title', None)
+    recipe = data.get('recipe', None)
+
+    try:
+        drink.title = title
+        drink.recipe = json.dumps(recipe, separators=(',', ':'))
+        drink.update()
+
+    except:
+        Drink.rollback()
+        abort(422)
+
+    return jsonify({
+        'success': True,
+        'drinks': [drink.long()]
+    })
+
+
 '''
-@TODO implement endpoint
+@DONE implement endpoint
     DELETE /drinks/<id>
         where <id> is the existing model id
         it should respond with a 404 error if <id> is not found
@@ -128,6 +156,24 @@ def post_drinks(payload):
     returns status code 200 and json {"success": True, "delete": id} where id is the id of the deleted record
         or appropriate status code indicating reason for failure
 '''
+
+
+@app.route('/drinks/<int:id>', methods=['DELETE'])
+@requires_auth('delete:drinks')
+def delete_drink(payload, id):
+    drink = Drink.query.get_or_404(id)
+
+    try:
+        drink.delete()
+
+    except:
+        Drink.rollback()
+        abort(422)
+
+    return jsonify({
+        'success': True,
+        'delete': drink.id
+    })
 
 
 # Error Handling
@@ -146,7 +192,7 @@ def unprocessable(error):
 
 
 '''
-@TODO implement error handlers using the @app.errorhandler(error) decorator
+@DONE implement error handlers using the @app.errorhandler(error) decorator
     each error handler should return (with approprate messages):
              jsonify({
                     "success": False, 
@@ -156,16 +202,67 @@ def unprocessable(error):
 
 '''
 
+
+@app.errorhandler(400)
+def bad_request(error):
+    return jsonify({
+        "success": False,
+        "error": 400,
+        "message": "bad request"
+    }), 400
+
+
+@app.errorhandler(405)
+def not_allowed(error):
+    return jsonify({
+        "success": False,
+        "error": 405,
+        "message": "method not allowed"
+    }), 405
+
+
+@app.errorhandler(500)
+def server_error(error):
+    return jsonify({
+        "success": False,
+        "error": 500,
+        "message": "internal server error"
+    }), 500
+
+
 '''
-@TODO implement error handler for 404
+@DONE implement error handler for 404
     error handler should conform to general task above 
 '''
 
 
+@app.errorhandler(404)
+def not_found(error):
+    return jsonify({
+        'success': False,
+        'error': 404,
+        'message': 'resource not found'
+    }), 404
+
+
 '''
-@TODO implement error handler for AuthError
+@DONE implement error handler for AuthError
     error handler should conform to general task above 
 '''
+
+
+@app.errorhandler(AuthError)
+def authentication_error(error):
+    if 'description' in error.error:
+        message = error.error['description']
+    else:
+        message = 'authentication error'
+    return jsonify({
+        'success': False,
+        'error': error.status_code,
+        'message': message
+    }), 401
+
 
 if __name__ == '__main__':
     app.run()
