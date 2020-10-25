ce53ebebf71af21c6bb2e46ae37e9ef34ab23fd9
Gabriel Brandão <gabriel.brandaoal@gmail.com>
2020-10-25 08:52:07 -0300

    Add api endpoints

--- capstone/starter/app.py
+++ capstone/starter/app.py
@@ -2,13 +2,235 @@ import os
 from flask import Flask, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+from auth import AuthError, requires_auth
+from models import Movie, Actor, setup_db
+
 
 def create_app(test_config=None):
-  # create and configure the app
-  app = Flask(__name__)
-  CORS(app)
+    # create and configure the app
+    app = Flask(__name__)
+    setup_db(app)
+    CORS(app)
 
-  return app
+    # API Endpoints
+    @app.route('/movies', methods=['GET'])
+    @requires_auth('get:movies')
+    def get_movies():
+        movies = Movie.query.all()
+
+        if len(movies) == 0:
+            abort(404)
+
+        return jsonify({
+            'success': True,
+            'drinks': [movie.format() for movie in movies]
+        })
+
+    @app.route('/movies', methods=['POST'])
+    @requires_auth('post:movies')
+    def post_movies(payload):
+        data = request.get_json()
+
+        if not data:
+            abort(400)
+
+        title = data.get('title', None)
+        release_date = data.get('release_date', None)
+
+        if None in [title, release_date]:
+            abort(422)
+
+        try:
+            new_movie = Movie(
+                title=title,
+                release_date=release_date
+            )
+            new_movie.insert()
+
+        except:
+            Movie.rollback()
+            abort(422)
+
+        return jsonify({
+            'success': True,
+            'created': new_movie.id
+        })
+
+    @app.route('/movies/<int:id>', methods=['PATCH'])
+    @requires_auth('patch:movies')
+    def patch_movies(payload, id):
+        movie = Movie.query.get_or_404(id)
+
+        data = request.get_json()
+
+        if not data:
+            abort(400)
+
+        title = data.get('title', None)
+        release_date = data.get('release_date', None)
+
+        try:
+            movie.title = title
+            movie.release_date = release_date
+            movie.update()
+
+        except:
+            Movie.rollback()
+            abort(422)
+
+        return jsonify({
+            'success': True,
+            'movie': [movie.format()]
+        })
+
+    @app.route('/movies/<int:id>', methods=['DELETE'])
+    @requires_auth('delete:movies')
+    def delete_movie(payload, id):
+        movie = Movie.query.get_or_404(id)
+
+        try:
+            movie.delete()
+
+        except:
+            Movie.rollback()
+            abort(422)
+
+        return jsonify({
+            'success': True,
+            'deleted': movie.id
+        })
+
+    @app.route('/actors', methods=['GET'])
+    @requires_auth('get:actors')
+    def get_actors():
+        actors = Movie.query.all()
+
+        if len(actors) == 0:
+            abort(404)
+
+        return jsonify({
+            'success': True,
+            'actors': [actor.format() for actor in actors]
+        })
+
+    @app.route('/actors', methods=['POST'])
+    @requires_auth('post:actors')
+    def post_actors(payload):
+        data = request.get_json()
+
+        if not data:
+            abort(400)
+
+        name = data.get('name', None)
+        age = data.get('age', None)
+        gender = data.get('gender', None)
+
+        if None in [name, age, gender]:
+            abort(422)
+
+        try:
+            new_actor = Actor(
+                name=name,
+                age=age,
+                gender=age
+            )
+            new_actor.insert()
+
+        except:
+            Actor.rollback()
+            abort(422)
+
+        return jsonify({
+            'success': True,
+            'created': new_actor.id
+        })
+
+    @app.route('/actors/<int:id>', methods=['PATCH'])
+    @requires_auth('patch:actors')
+    def patch_actors(payload, id):
+        actor = Actor.query.get_or_404(id)
+
+        data = request.get_json()
+
+        if not data:
+            abort(400)
+
+        name = data.get('name', None)
+        age = data.get('age', None)
+        gender = data.get('gender', None)
+
+        try:
+            actor.name = name
+            actor.age = age
+            actor.gender = gender
+            actor.update()
+
+        except:
+            Actor.rollback()
+            abort(422)
+
+        return jsonify({
+            'success': True,
+            'actor': [actor.format()]
+        })
+
+    @app.route('/actors/<int:id>', methods=['DELETE'])
+    @requires_auth('delete:actors')
+    def delete_actor(payload, id):
+        actor = Actor.query.get_or_404(id)
+
+        try:
+            actor.delete()
+
+        except:
+            Actor.rollback()
+            abort(422)
+
+        return jsonify({
+            'success': True,
+            'deleted': actor.id
+        })
+
+    # Error Handling
+
+    @app.errorhandler(422)
+    def unprocessable(error):
+        return jsonify({
+            "success": False,
+            "error": 422,
+            "message": "unprocessable"
+        }), 422
+
+    @app.errorhandler(400)
+    def bad_request(error):
+        return jsonify({
+            "success": False,
+            "error": 400,
+            "message": "bad request"
+        }), 400
+
+    @app.errorhandler(404)
+    def not_found(error):
+        return jsonify({
+            'success': False,
+            'error': 404,
+            'message': 'resource not found'
+        }), 404
+
+    @app.errorhandler(AuthError)
+    def authentication_error(error):
+        if 'description' in error.error:
+            message = error.error['description']
+        else:
+            message = 'authentication error'
+        return jsonify({
+            'success': False,
+            'error': error.status_code,
+            'message': message
+        }), error.status_code
+
+    return app
+
 
 APP = create_app()
 
--- capstone/starter/models.py
+++ capstone/starter/models.py
@@ -1,5 +1,5 @@
 import os
-from sqlalchemy import Column, String, Integer
+from sqlalchemy import Column, String, Integer, Date
 from flask_sqlalchemy import SQLAlchemy
 import json
 
@@ -16,7 +16,7 @@ setup_db(app)
 
 
 def setup_db(app):
-    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_uri
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
     db.init_app(app)
@@ -26,7 +26,6 @@ def setup_db(app):
 db_drop_and_create_all()
     drops the database tables and starts fresh
     can be used to initialize a clean database
-    !!NOTE you can change the database_filename variable to have multiple verisons of a database
 '''
 
 
@@ -39,3 +38,68 @@ class Movie(db.Model):
     __tablename__ = 'movies'
 
     id = Column(Integer, primary_key=True)
+    title = Column(String)
+    release_date = Column(Date)
+
+    def __init__(self, title, release_date):
+        self.title = title
+        self.release_date = release_date
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    @staticmethod
+    def rollback():
+        db.session.rollback()
+
+    def format(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'release_date': self.release_date
+        }
+
+
+class Actor(db.Model):
+    __tablename__ = 'actors'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    age = Column(Integer)
+    gender = Column(String)
+
+    def __init__(self, name, age, gender):
+        self.name = name
+        self.age = age
+        self.gender = gender
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    @staticmethod
+    def rollback():
+        db.session.rollback()
+
+    def format(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'age': self.age,
+            'gender': self.gender
+        }
